0f95213f96a31e27ae06547b9e042eb0b2d6cefc
Josh Crozier <crozier4@gmail.com>
2021-12-26 14:18:28 -0500

    Add solution #9

--- solutions/0009-palindrome-number.js
+++ solutions/0009-palindrome-number.js
@@ -4,9 +4,9 @@
  * Difficulty: Easy
  *
  * Given an integer `x`, return `true` if `x` is palindrome integer.
-
-An integer is a palindrome when it reads the same backward as forward.
-For example, `121` is palindrome while `123` is not.
+ *
+ * An integer is a palindrome when it reads the same backward as forward.
+ * - For example, `121` is palindrome while `123` is not.
  */
 
 /**
