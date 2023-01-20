f4d8da73d2d47cae64bf01bece0775d76c2e92ba
Josh Crozier <crozier4@gmail.com>
2023-01-19 22:50:12 -0600

    Add solution #884

--- solutions/0884-uncommon-words-from-two-sentences.js
+++ solutions/0884-uncommon-words-from-two-sentences.js
@@ -1,16 +1,16 @@
 /**
  * 884. Uncommon Words from Two Sentences
- * https://leetcode.com/problems/uncommon-words-from-two-sentences/submissions/
+ * https://leetcode.com/problems/uncommon-words-from-two-sentences/
  * Difficulty: Easy
  *
- * A sentence is a string of single-space separated words where each word consists
- * only of lowercase letters.
+ * A sentence is a string of single-space separated words where each word
+ * consists only of lowercase letters.
  *
- * A word is uncommon if it appears exactly once in one of the sentences, and does
- * not appear in the other sentence.
+ * A word is uncommon if it appears exactly once in one of the sentences,
+ * and does not appear in the other sentence.
  *
- * Given two sentences s1 and s2, return a list of all the uncommon words. You may
- * return the answer in any order.
+ * Given two sentences s1 and s2, return a list of all the uncommon words.
+ * You may return the answer in any order.
  */
 
 /**
@@ -20,7 +20,6 @@
  */
 var uncommonFromSentences = function(s1, s2) {
   const map = new Map();
-
   (s1 + ' ' + s2).split(/\s+/).forEach(s => map.set(s, (map.get(s) || 0) + 1));
   return [...map].reduce((a, [k, c]) => c === 1 ? [...a, k] : a, []);
 };
