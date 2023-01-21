ffb787f538de9efcc00f21e4fa636c591f06e2bc
Josh Crozier <crozier4@gmail.com>
2023-01-20 21:12:46 -0600

    Add solution #4

--- solutions/0004-median-of-two-sorted-arrays.js
+++ solutions/0004-median-of-two-sorted-arrays.js
@@ -17,33 +17,9 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-  const total = nums1.length + nums2.length;
-  const limit = Math.floor(total / 2) + 1;
-  let i = 0, j = 0, prev, last
-
-  while (i + j < limit) {
-    if (last !== undefined) {
-      prev = last;
-    }
-    if (nums1[i] < nums2[j] || j === nums2.length) {
-      last = nums1[i++];
-    } else {
-      last = nums2[j++];
-    }
-  }
-
-  return total % 2 === 0 ? (prev + last) / 2 : last;
-};
-
-// shorter/readable alternative with higher time complexity:
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
- */
-var findMedianSortedArrays = function(nums1, nums2) {
   const sorted = [...nums1, ...nums2].sort((a, b) => a - b);
   const index = Math.floor((sorted.length - 1) / 2);
+
   return sorted.length % 2 === 0
     ? (sorted[index] + sorted[index + 1]) / 2
     : sorted[index];
