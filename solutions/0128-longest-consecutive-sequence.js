150d179038e42ce5826c8094db205b27065202c6
Josh Crozier <crozier4@gmail.com>
2025-02-25 16:23:33 -0600

    Add solution #128

--- solutions/0128-longest-consecutive-sequence.js
+++ solutions/0128-longest-consecutive-sequence.js
@@ -3,8 +3,8 @@
  * https://leetcode.com/problems/longest-consecutive-sequence/
  * Difficulty: Medium
  *
- * Given an unsorted array of integers nums, return the length of the
- * longest consecutive elements sequence.
+ * Given an unsorted array of integers nums, return the length of the longest consecutive
+ * elements sequence.
  *
  * You must write an algorithm that runs in O(n) time.
  */
@@ -17,13 +17,16 @@ var longestConsecutive = function(nums) {
   const set = new Set(nums);
   let result = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (!set.has(nums[i] - 1)) {
-      let streak = 1;
-      while (set.has(nums[i] + streak)) {
-        streak++;
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let count = 1;
+      let n = num;
+
+      while (set.has(n + 1)) {
+        n++;
+        count++;
       }
-      result = Math.max(result, streak);
+      result = Math.max(result, count);
     }
   }
 
