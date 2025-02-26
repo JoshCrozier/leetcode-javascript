ce6ae8e25df65b26ed352fb1d584ed3ba01cf0de
Josh Crozier <crozier4@gmail.com>
2025-02-25 18:23:56 -0600

    Add solution #15

--- solutions/0015-3sum.js
+++ solutions/0015-3sum.js
@@ -3,8 +3,8 @@
  * https://leetcode.com/problems/3sum/
  * Difficulty: Medium
  *
- * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j,
- * i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+ * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
  *
  * Notice that the solution set must not contain duplicate triplets.
  */
@@ -14,25 +14,30 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  const result = new Set();
-
+  const result = [];
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
     let j = i + 1;
     let k = nums.length - 1;
-
     while (j < k) {
       const sum = nums[i] + nums[j] + nums[k];
       if (!sum) {
-        result.add(JSON.stringify([nums[i], nums[j++], nums[k--]]));
-      } else if (sum > 0) {
-        k--;
-      } else {
+        result.push([nums[i], nums[j], nums[k]]);
         j++;
+        k--;
+        while (j < k && nums[j] === nums[j - 1]) {
+          j++;
+        }
+        while (j < k && nums[k] === nums[k + 1]) {
+          k--;
+        }
+      } else {
+        sum < 0 ? j++ : k--;
       }
     }
   }
 
-  return [...result].map(JSON.parse);
+  return result;
 };
