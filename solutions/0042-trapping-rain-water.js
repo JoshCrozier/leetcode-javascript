db9387bd40df25ec35bc5437c6482128c5d44cf0
Josh Crozier <crozier4@gmail.com>
2025-02-25 18:18:54 -0600

    Add solution #42

--- solutions/0042-trapping-rain-water.js
+++ solutions/0042-trapping-rain-water.js
@@ -3,8 +3,8 @@
  * https://leetcode.com/problems/trapping-rain-water/
  * Difficulty: Hard
  *
- * Given `n` non-negative integers representing an elevation map where the width
- * of each bar is `1`, compute how much water it can trap after raining.
+ * Given n non-negative integers representing an elevation map where the
+ * width of each bar is 1, compute how much water it can trap after raining.
  */
 
 /**
@@ -12,23 +12,12 @@
  * @return {number}
  */
 var trap = function(height) {
-  const leftMax = [];
-  const rightMax = [];
   let result = 0;
 
-  leftMax[0] = height[0];
-  rightMax[height.length - 1] = height[height.length - 1];
-
-  for (let i = 1; i < height.length; i++) {
-    leftMax[i] = Math.max(height[i], leftMax[i - 1]);
-  }
-
-  for (let i = height.length - 2; i > -1; i--) {
-    rightMax[i] = Math.max(height[i], rightMax[i + 1]);
-  }
-
-  for (let i = 0; i < height.length; i++) {
-    result += Math.min(leftMax[i], rightMax[i]) - height[i];
+  for (let left = 0, right = height.length - 1, maxL = 0, maxR = 0; left < right;) {
+    maxL = Math.max(maxL, height[left]);
+    maxR = Math.max(maxR, height[right]);
+    result += maxL < maxR ? maxL - height[left++] : maxR - height[right--];
   }
 
   return result;
