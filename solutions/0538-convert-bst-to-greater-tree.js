ce55ae4a5c0e21584f1e0d87c78fb12908a4a0ae
Josh Crozier <crozier4@gmail.com>
2025-03-05 01:10:50 -0600

    Add solution #538

---
+++ solutions/0538-convert-bst-to-greater-tree.js
@@ -0,0 +1,40 @@
+/**
+ * 538. Convert BST to Greater Tree
+ * https://leetcode.com/problems/convert-bst-to-greater-tree/
+ * Difficulty: Medium
+ *
+ * Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every
+ * key of the original BST is changed to the original key plus the sum of all keys greater
+ * than the original key in BST.
+ *
+ * As a reminder, a binary search tree is a tree that satisfies these constraints:
+ * - The left subtree of a node contains only nodes with keys less than the node's key.
+ * - The right subtree of a node contains only nodes with keys greater than the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+  let sum = 0;
+  traverse(root);
+  return root;
+
+  function traverse(node) {
+    if (!node) return;
+    traverse(node.right);
+    sum += node.val;
+    node.val = sum;
+    traverse(node.left);
+  }
+};
